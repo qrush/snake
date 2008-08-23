612f18186f2fe0f80648fa05758f27b9f539e510
Nick Quaranto <nick@quaran.to>
2008-08-22 23:15:04 -0400

    Can i has rectangles?

--- snake.rb
+++ snake.rb
@@ -1,5 +1,9 @@
 
 class Field 
+  CELL_SIZE = 15
+  START_X = 35
+  START_Y = 100
+  SIDE = 25
   
   def initialize(app)
     @app = app
@@ -15,18 +19,37 @@ class Field
 =end
   end
 
+  def render_cell(x, y, color = "#46350A", stroke = true)
+    
+    @app.stroke "#fff"
+    @app.fill color
+    @app.rect x, y, CELL_SIZE, CELL_SIZE
+    #cell_size = 10
+    #if stroke  
+    #@app.fill color
+    #@app.rect x*cell_size, y*cell_size, cell_size-1, cell_size-1
+    #@app.stroke "#BBB" if stroke
+    #@app.line x*cell_size+1, y*cell_size+1, x*cell_size+cell_size-1, y*cell_size
+    #@app.line x*cell_size+1, y*cell_size+1, x*cell_size, y*cell_size+cell_size-1
+  end
+
   def paint
-  
+    
+    SIDE.times do |i|
+      render_cell(START_X + ((CELL_SIZE+2) * i), START_Y)
+    end
+    
   end
 
 end
 
 
-Shoes.app :height => 500, :width => 500, :title => "It's a Snake!" do
+Shoes.app :height => 500, :width => 500, :title => "Snakes" do
   background "#08ab2e".."#1c582a"
   
-  
   @field = Field.new(self)
+  @field.paint
+  animate(1) { @status.replace "Time: #{Time.now.strftime('%T')}" }
   
   flow :margin => 4 do
     button("Beginner") { new_game :beginner }
@@ -36,9 +59,6 @@ Shoes.app :height => 500, :width => 500, :title => "It's a Snake!" do
   
   stack do @status = para :stroke => white end
   
-  animate(5) { @status.replace "Time: #{Time.now}" }
-  
-  @field.paint
   para "Left click - open cell, right click - put flag, middle click - reveal empty cells", :top => 420, :left => 0, :stroke => white,  :font => "11px"
 end
 
